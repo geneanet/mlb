cb65729d714467188e5889681fc03029bbfaf5dd
Hervé BRY <herve.bry@geneanet.org>
2023-08-24 11:09:06 +0200

    Fix meta JSON export

--- backend/meta.go
+++ backend/meta.go
@@ -1,7 +1,10 @@
 package backend
 
 import (
+	"encoding/json"
+
 	"github.com/zclconf/go-cty/cty"
+	ctyjson "github.com/zclconf/go-cty/cty/json"
 )
 
 // Map
@@ -69,4 +72,14 @@ func (m MetaBucket) Clone() MetaBucket {
 		new[k] = v
 	}
 	return new
+}
+
+func (m MetaBucket) MarshalJSON() ([]byte, error) {
+	b := map[string]ctyjson.SimpleJSONValue{}
+
+	for k, v := range m {
+		b[k] = ctyjson.SimpleJSONValue{Value: v}
+	}
+
+	return json.Marshal(b)
 }
